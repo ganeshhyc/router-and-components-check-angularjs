96f60be6446ec1f6677a84600960bc0f703131d7
Ganesh Kumar <32690780+ganeshhyc@users.noreply.github.com>
2019-12-03 20:07:42 +0530

    Update landing.js

--- app/controller/landing.js
+++ app/controller/landing.js
@@ -4,7 +4,7 @@ app.controller("landingCtrl",function($scope,$http){
     $scope.countryList  = countries;
     $scope.checkW = function(city){
         if(city!==undefined)
-        $http.get("http://api.openweathermap.org/data/2.5/find?q="+city+"&appid=f18817a8b4022410d79d5adbd5d89746").
+        $http.get("http://api.openweathermap.org/data/2.5/find?q="+city+"&appid=YOUR_API_KEY").
         then(function successCallback(response){
             $scope.city[city] = response.data;
         },
